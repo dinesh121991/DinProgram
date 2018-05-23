fbde6327d3b5680b0dab478bd9af7937c97fea0b
Dineshkumar RAJAGOPAL <dineshkumar.2.rajagopal@atos.net>
2018-05-23 15:14:54 +0200

    Err: Line 28 return value errorwq

--- Shell/function.sh
+++ Shell/function.sh
@@ -18,3 +18,26 @@ echo "Global: $sample_text"
 
 foo "$a"
 echo "Global: $sample_text"
+
+yes_or_no() {
+	while true
+	do
+		echo "Is your name $* ?"
+		read status
+		case "$status" in
+			[yY] | [yY][eE][sS]) return 1;;
+			[nN] | [nN][oO]) return 0;;
+			* ) echo "Enter Yes / No only..."
+		esac
+	done
+}
+
+echo "Enter Your Name"
+read name
+
+if yes_or_no "$name"
+then
+	echo "Yes, You are $name"
+else
+	echo "Who you are?"
+fi
