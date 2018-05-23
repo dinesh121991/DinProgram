84070137e0dc3f02ccc44ba86f003cfc58d96f06
Dineshkumar RAJAGOPAL <dineshkumar.2.rajagopal@atos.net>
2018-05-23 15:00:32 +0200

    Line 19 var:a value is val:Dinesh-

--- Shell/function.sh
+++ Shell/function.sh
@@ -5,4 +5,16 @@ foo() {
 foo 32
 b=2
 a="$(foo $b)"
+echo $a
 echo "Main: $a"
+
+sample_text="Global Variable"
+echo "Global: $sample_text"
+foo() {
+	local sample_text="Local Variable"
+	echo "Local: $sample_text Param: $0 $1"
+}
+echo "Global: $sample_text"
+
+foo $a
+echo "Global: $sample_text"
