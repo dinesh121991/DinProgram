c300f4a7420f230ce918667d6285af1d067fab13
dinesh <dinesh121991@gmail.com>
2019-05-03 13:50:14 +0200

    Ch - 10 contact form

--- Web/Django/chapter_6/chapter_6/settings.py
+++ Web/Django/chapter_6/chapter_6/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'z(n#_ek4o8kzo5ks2klai(sdz6f87ja2uoczy-fl!f9_qmq%!l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost']
 
--- Web/Django/chapter_6/pages/views.py
+++ Web/Django/chapter_6/pages/views.py
@@ -3,7 +3,9 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render, get_object_or_404
 from . models import Page
-#from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.mail import send_mail, get_connection
+from .forms import ContactForm
 
 def index(request, pageName):
     print("Inside rendering")
@@ -20,3 +22,28 @@ def index(request, pageName):
     return render(request, "pages/pages.html", context)
 
 # Create your views here.
+
+
+def contact(request):
+    submitted = False
+    print(request.method)
+    if (request.method == 'POST'):
+#    if (request.method == 'POST') and ('exist' in request.POST):
+        form = ContactForm(request.POST)
+	print(request.POST)
+        print(form)
+        print(type(form))
+        if (form.is_valid()):
+            cd = form.cleaned_data
+            con = get_connection('django.core.mail.backends.console.EmailBackend')
+            send_mail(cd['subject'], cd['content'], cd.get('email', 'dinesh121991@gmail.com'), ['dinesh121991@gmail.com'], connection=con)
+            return HttpResponseRedirect('/contact?submitted=True')
+    else:
+        form = ContactForm()
+        print(form)
+        print(type(form))
+        print(request.GET)
+        if 'submitted' in request.GET:
+            submitted = True
+    return render(request, 'pages/contact.html', {'form': form, 'submitted': submitted, 'page_list': Page.objects.all()})
+
